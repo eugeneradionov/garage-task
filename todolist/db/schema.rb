c650b25341c8193285375eede5d5e07e8a8537bf
Eugene Radionov <eugene.radionov@gmail.com>
2017-03-04 13:45:20 +0200

    Added deadline for tasks

--- todolist/db/schema.rb
+++ todolist/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228170852) do
+ActiveRecord::Schema.define(version: 20170302175433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170228170852) do
     t.datetime "updated_at",                 null: false
     t.boolean  "completed",  default: false
     t.integer  "priority",   default: 1
+    t.date     "deadline"
     t.index ["project_id"], name: "index_tasks_on_project_id", using: :btree
   end
 
