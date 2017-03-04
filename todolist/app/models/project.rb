414bbca464533e13a249972692c15a19db8c684d
Eugene Radionov <eugene.radionov@gmail.com>
2017-03-04 11:58:52 +0200

    Length validation for Project and Task added.

--- todolist/app/models/project.rb
+++ todolist/app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, length: {in: 1..200}
+  validates_associated :tasks
 end
--- todolist/app/models/task.rb
+++ todolist/app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project
-  validates :name, presence: true
+  validates :name, presence: true, length: {in: 1..1000}
 
   default_scope -> { order ('created_at DESC') }
   scope :completed, -> { where(completed: true) }
